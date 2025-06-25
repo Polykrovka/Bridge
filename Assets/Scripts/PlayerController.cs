b4d45a7c5b1b7f315e0facf7e1229043c6cec01a
Anton Nedvedz <2595092@gmail.com>
2025-06-25 18:16:28 +0200

    Add rotation lader

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,9 @@ public class PlayerController:MonoBehaviour
 
     public Transform ladder;
     public float ladderGrowSpeed = 2f;
+    public float dropRotateSpeed = 90f;
     private bool isGrowing = false;
+    private bool isDropping = false;
 
     private void Awake()
     {
@@ -18,7 +20,11 @@ public class PlayerController:MonoBehaviour
 
         // Subscribe to input events to start/stop ladder growing
         controls.Player.Interact.started += ctx => isGrowing = true;
-        controls.Player.Interact.canceled += ctx => isGrowing = false;
+        controls.Player.Interact.canceled += ctx =>
+        {
+            isGrowing = false;
+            isDropping = true; 
+        };
     }
 
     void Start()
@@ -45,7 +51,11 @@ public class PlayerController:MonoBehaviour
             // Move ladder down by half of growAmount to keep bottom fixed
             ladder.localPosition += new Vector3(0, growAmount / 2f, 0);
         }
-
+        else if(isDropping && ladder != null)
+        {
+            float step = dropRotateSpeed * Time.deltaTime;
+            ladder.Rotate(0, 0, -step);
+        }
     }
 
     private void OnEnable()
